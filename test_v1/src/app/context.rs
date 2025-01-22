96ad6f073bb868498f8f1936c84b3b18261939e9
Edwin Burwell <edwinburwell758@gmail.com>
2025-01-22 13:13:28 -0600

    Draw multiple triangles.

--- test_v1/src/app/context.rs
+++ test_v1/src/app/context.rs
@@ -1,7 +1,67 @@
 use std::{borrow::Cow, sync::Arc};
 
+use bytemuck::{Pod, Zeroable};
+use pollster::block_on;
+use util::DeviceExt as _;
 use wgpu::*;
-use winit::{dpi::PhysicalSize, event_loop::ActiveEventLoop, window::Window};
+use winit::{
+    dpi::{LogicalSize, PhysicalSize},
+    event_loop::ActiveEventLoop,
+    window::{Window, WindowAttributes},
+};
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Pod, Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    fn desc() -> VertexBufferLayout<'static> {
+        VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as BufferAddress,
+            step_mode: VertexStepMode::Vertex,
+            attributes: &[
+                VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: VertexFormat::Float32x3,
+                },
+                VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as BufferAddress,
+                    shader_location: 1,
+                    format: VertexFormat::Float32x3,
+                },
+            ],
+        }
+    }
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [-0.0868241, 0.49240386, 0.0],
+        color: [1.0, 0.0, 0.0],
+    }, // A
+    Vertex {
+        position: [-0.49513406, 0.06958647, 0.0],
+        color: [0.0, 1.0, 0.0],
+    }, // B
+    Vertex {
+        position: [-0.21918549, -0.44939706, 0.0],
+        color: [0.0, 0.0, 1.0],
+    }, // C
+    Vertex {
+        position: [0.35966998, -0.3473291, 0.0],
+        color: [1.0, 1.0, 1.0],
+    }, // D
+    Vertex {
+        position: [0.44147372, 0.2347359, 0.0],
+        color: [0.0, 0.0, 0.0],
+    }, // E
+];
+
+const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
 
 #[allow(dead_code)]
 pub(crate) struct Context {
@@ -15,11 +75,20 @@ pub(crate) struct Context {
     pipeline_layout: PipelineLayout,
     render_pipeline: RenderPipeline,
     config: SurfaceConfiguration,
+    vertex_buffer: Buffer,
+    index_buffer: Buffer,
 }
 
 impl Context {
     pub(crate) fn new(event_loop: &ActiveEventLoop) -> Self {
-        let window = Arc::new(event_loop.create_window(Default::default()).unwrap());
+        let window = Arc::new(
+            event_loop
+                .create_window(WindowAttributes::default().with_inner_size(LogicalSize {
+                    width: 500,
+                    height: 500,
+                }))
+                .unwrap(),
+        );
 
         let mut size = window.inner_size();
         size.width = size.width.max(1);
@@ -32,14 +101,14 @@ impl Context {
         });
 
         let surface = instance.create_surface(window.clone()).unwrap();
-        let adapter = pollster::block_on(instance.request_adapter(&RequestAdapterOptions {
+        let adapter = block_on(instance.request_adapter(&RequestAdapterOptions {
             power_preference: PowerPreference::default(),
             force_fallback_adapter: false,
             compatible_surface: Some(&surface),
         }))
         .unwrap();
 
-        let (device, queue) = pollster::block_on(adapter.request_device(
+        let (device, queue) = block_on(adapter.request_device(
             &DeviceDescriptor {
                 label: None,
                 required_features: Features::empty(),
@@ -71,7 +140,7 @@ impl Context {
             vertex: VertexState {
                 module: &shader,
                 entry_point: Some("vs_main"),
-                buffers: &[],
+                buffers: &[Vertex::desc()],
                 compilation_options: Default::default(),
             },
             fragment: Some(FragmentState {
@@ -92,6 +161,18 @@ impl Context {
             .unwrap();
         surface.configure(&device, &config);
 
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(VERTICES),
+            usage: BufferUsages::VERTEX,
+        });
+
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: BufferUsages::INDEX,
+        });
+
         Context {
             window,
             instance,
@@ -103,6 +184,8 @@ impl Context {
             pipeline_layout,
             render_pipeline,
             config,
+            vertex_buffer,
+            index_buffer,
         }
     }
 
@@ -114,13 +197,18 @@ impl Context {
             .create_command_encoder(&CommandEncoderDescriptor { label: None });
 
         {
-            let mut rpass = encoder.begin_render_pass(&RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
                 label: None,
                 color_attachments: &[Some(RenderPassColorAttachment {
                     view: &view,
                     resolve_target: None,
                     ops: Operations {
-                        load: LoadOp::Clear(Color::BLUE),
+                        load: LoadOp::Clear(Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
                         store: StoreOp::Store,
                     },
                 })],
@@ -129,8 +217,10 @@ impl Context {
                 occlusion_query_set: None,
             });
 
-            rpass.set_pipeline(&self.render_pipeline);
-            rpass.draw(0..3, 0..1);
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_index_buffer(self.index_buffer.slice(..), IndexFormat::Uint16);
+            render_pass.draw_indexed(0..INDICES.len() as u32, 0, 0..1);
         }
 
         self.queue.submit(Some(encoder.finish()));
